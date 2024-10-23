ce9f6dfa8b1fc9c38a0916fe9d7520564f58289f
MrXiwlev <mrxiwlev@gmail.com>
2024-10-23 16:28:02 +0300

    update service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,37 +7,36 @@ const urlsToCache = [
   "/icons/icon-512x512.png",
 ];
 
-// Install the service worker and cache assets
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log("Opened cache");
-      return cache.addAll(urlsToCache);
-    })
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+      .catch((error) => console.error("Cache open failed: ", error))
   );
 });
 
-// Intercept requests and serve cached content when offline
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
-      // Cache hit - return the cached response
       if (response) {
         return response;
       }
-      return fetch(event.request);
+      return fetch(event.request).catch((error) => {
+        console.error("Fetching failed:", error);
+        throw error;
+      });
     })
   );
 });
 
-// Activate the service worker and remove old caches
 self.addEventListener("activate", (event) => {
   const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
         cacheNames.map((cacheName) => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
+          if (!cacheWhitelist.includes(cacheName)) {
             return caches.delete(cacheName);
           }
         })

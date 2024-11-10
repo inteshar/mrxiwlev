cb1baff304a507f7aefa1ad1a0ef229edc9bdf55
MrXiwlev <mrxiwlev@gmail.com>
2024-11-10 13:45:27 +0300

    update project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,6 +3,8 @@ import p11 from "../assets/p11.png";
 import p12 from "../assets/p11.gif";
 import p21 from "../assets/p21.png";
 import p22 from "../assets/p22.gif";
+import p31 from "../assets/p31.png";
+import p32 from "../assets/p32.gif";
 
 const Projects = () => {
   return (
@@ -154,7 +156,7 @@ const Projects = () => {
                     <div className="w-full h-full flex justify-center overflow-x-auto snap-x">
                       <img
                         src={p22}
-                        className="object-cover snap-center shadow-sm border border-[#6f6b2a] rounded-lg mb-2 w-max sm:h-full h-[fit]"
+                        className="object-cover snap-center shadow-sm border border-[#6f6b2a] rounded-lg mb-2 w-max h-[fit]"
                         alt=""
                       />
                     </div>
@@ -219,6 +221,137 @@ const Projects = () => {
                     forms. Yup schema ensures validation. Its intuitive
                     interface enables secure password creation tailored to
                     preferences.
+                  </p>
+                </details>
+              </div>
+              <hr />
+            </li>
+            <li>
+              <hr className="hidden sm:block" />
+              <div className="timeline-middle hidden sm:block">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  className="h-5 w-5 text-[#6f6b2a]"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </div>
+              <div className="w-full timeline-start flex flex-col sm:items-end mb-10 border border-[#6f6b2a] rounded-lg p-4 bg-[#f9f7ec] drop-shadow-lg">
+                <div className="relative w-full h-full">
+                  <img
+                    src={p31}
+                    className="shadow-sm border border-[#6f6b2a] rounded-lg mb-2"
+                    alt=""
+                  />
+                  <div className="absolute inset-0 flex justify-center items-center opacity-0 hover:opacity-100 transition-opacity duration-300">
+                    <div className="w-full h-full flex justify-center overflow-x-auto snap-x">
+                      <img
+                        src={p32}
+                        className="object-cover snap-center shadow-sm border border-[#6f6b2a] rounded-lg mb-2 w-max h-[fit]"
+                        alt=""
+                      />
+                    </div>
+                  </div>
+                </div>
+                <time className="font-mono italic text-sm">2024</time>
+                <div className="text-lg font-black">Room Rental MS</div>
+                <ul className="flex gap-2 sm:text-sm text-[8px] mt-2">
+                  <li className="bg-[#e0dbbb] sm:px-4 sm:py-1 w-max px-2 py-1 font-semibold rounded-lg text-gray-600 shadow-sm">
+                    React
+                  </li>
+                  <li className="bg-[#e0dbbb] sm:px-4 sm:py-1 w-max px-2 py-1 font-semibold rounded-lg text-gray-600 shadow-sm">
+                    Firebase
+                  </li>
+                  <li className="bg-[#e0dbbb] sm:px-4 sm:py-1 w-max px-2 py-1 font-semibold rounded-lg text-gray-600 shadow-sm">
+                    Styled Components
+                  </li>
+                  <li className="bg-[#e0dbbb] sm:px-4 sm:py-1 w-max px-2 py-1 font-semibold rounded-lg text-gray-600 shadow-sm">
+                    React Components
+                  </li>
+                  <li className="bg-[#e0dbbb] sm:px-4 sm:py-1 w-max px-2 py-1 font-semibold rounded-lg text-gray-600 shadow-sm">
+                    CSS
+                  </li>
+                </ul>
+                <div>
+                  <ul className="flex gap-2 mt-2">
+                    <li>
+                      <a
+                        href="https://github.com/inteshar/room-rental-ms"
+                        className="hover:custom-cursor-hover"
+                        target="_blank"
+                      >
+                        <img
+                          width="24"
+                          height="24"
+                          src="https://img.icons8.com/3d-fluency/94/github.png"
+                          alt="github"
+                        />
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="https://roomrentalms.vercel.app/"
+                        className="hover:custom-cursor-hover"
+                        target="_blank"
+                      >
+                        <img
+                          width="24"
+                          height="24"
+                          src="https://img.icons8.com/3d-fluency/94/link.png"
+                          alt="github"
+                        />
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+                <details className="mt-2">
+                  <summary className="hover:custom-cursor-hover font-bold text-[#6f6b2a] sm:text-sm text-xs">
+                    Show more
+                  </summary>
+                  <p className="text-justify">
+                    RoomRental is a comprehensive web-based system designed to
+                    streamline property rental management. It offers tools to
+                    handle rooms, tenants, bookings, finances, and more, making
+                    it easier to manage rental operations from one place.
+                    <br />
+                    <br />
+                    <span className="font-bold">Features</span>
+                    <ul>
+                      <li>
+                        Room Management: Track room availability, assign
+                        tenants, and manage occupancy.
+                      </li>
+                      <li>
+                        Tenant Management: Maintain tenant records, contacts,
+                        and payment history.
+                      </li>
+                      <li>
+                        Booking Management: Schedule and track bookings,
+                        renewals, and cancellations.
+                      </li>
+                      <li>
+                        Financial Management: Automate billing, track payments,
+                        and generate financial reports.
+                      </li>
+                      <li>
+                        Maintenance & Complaints: Manage maintenance requests
+                        and complaints with a tracking system.
+                      </li>
+                      <li>
+                        Analytics & Reporting: Get insights into revenue,
+                        occupancy rates, and tenant behavior.
+                      </li>
+                      <li>
+                        Mobile Responsiveness: Optimized for mobile use to
+                        manage on-the-go.
+                      </li>
+                    </ul>
                   </p>
                 </details>
               </div>

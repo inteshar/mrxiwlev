4e7cd762b24c12e4b2e7763b3dac32b2fdb39ca7
MrXiwlev <mrxiwlev@gmail.com>
2024-12-31 17:02:25 +0300

    update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,6 @@
+// Use ES module import syntax instead of require
+import daisyui from 'daisyui';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -16,5 +19,5 @@ export default {
       outfit: ["Outfit", "sans-serif"],
     },
   },
-  plugins: [require("daisyui")],
+  plugins: [daisyui],
 };
